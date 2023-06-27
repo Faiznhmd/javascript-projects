4bf421a6f13bfe45d7681c9b7b6b1de2f4bf3f71
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-06-27 18:01:21 +0530

    Random quote generater

--- Random Quote Generater/script.js
+++ Random Quote Generater/script.js
@@ -1,5 +1,6 @@
 let quote = document.getElementById("quote");
 let author = document.getElementById("author");
+let writer = document.getElementById("writer");
 let btn = document.getElementById("btn");
 
 const url = "https://api.quotable.io/random";
@@ -10,6 +11,7 @@ let getQuote = () => {
     .then((item) => {
       quote.innerText = item.content;
       author.innerText = item.author;
+      writer.innerText = item.authorSlug;
     });
 };
 
